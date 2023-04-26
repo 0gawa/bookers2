6a6f6846ee6651b95984b99130764aad96bffd74
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2023-04-26 15:55:24 +0000

    投稿フォームの作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,5 +3,7 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
+    @user = User.find(current_user.id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @profile_image = @user.profile_image
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
