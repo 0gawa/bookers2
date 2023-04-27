c3eb497f1aac274cd0396add3a4240b7bc2a28b3
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2023-04-27 15:21:10 +0000

    投稿機能制作完了&レイアウト制作途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
   end
+  
+  def index
+    @user = User.find(current_user.id)
+    @users = User.all
+  end
+  
   
   private
 
