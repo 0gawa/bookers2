0352e8732781c61c8845f4ebcec98a92f24db3c2
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2023-04-29 17:02:17 +0000

    [Finish]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
     @user = User.find(current_user.id)
   end
   
@@ -16,10 +17,31 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    flash[:notice] = "making a book is error."
     @book.save
+    flash[:notice] = "making a book is successfully."
     redirect_to book_path(@book.id)
   end
-
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    flash[:notice] = "updating a book is error."
+    @book.update(book_params)
+    flash[:notice] = "updating a book is successfully."
+    redirect_to book_path(params[:id])
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  
+  
   private
 
   def book_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def new
-   @book = Book.new
+   @user = User.new
   end
   
   def show
@@ -16,7 +16,9 @@ class UsersController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    flash[:notice] = "making a book is error."
     @book.save
+    flash[:notice] = "making a book is successfully."
     redirect_to book_path(@book.id)
   end
   
@@ -27,7 +29,9 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
+    flash[:notice] = "updating a book is error."
     @user.update(user_params)
+    flash[:notice] = "updating user's information is successfully."
     redirect_to user_path(@user.id)
   end
   
@@ -35,11 +39,11 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
   
   def book_params
-    params.require(:book).permit(:title, :profile_image, :body)
+    params.require(:book).permit(:title, :body)
   end
   
 end
