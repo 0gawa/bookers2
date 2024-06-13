0172e71084b6c10481cf27ea15fd3faa2879455f
Ogawa <ogawadaiki@ogawadaikinoMacBook-Air.local>
2024-06-14 00:57:17 +0900

    [実装]課題9CMail機能

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -41,6 +41,23 @@ class GroupsController < ApplicationController
         end
     end
 
+    def new_send_mail
+        @group = Group.find(params[:group_id])
+    end
+
+    def send_notice_mail
+        group = Group.find(params[:group_id])
+        user = User.find(group.owner_id)
+        content = params[:content]
+        UserMailer.send_notice_mail(user, content, params[:title]).deliver
+        redirect_to confirm_mail_path(group_id: group.id,title: params[:title], content: params[:content])
+    end
+
+    def confirm_mail
+        @title = params[:title]
+        @content = params[:content]
+    end
+
     private
   
     def group_params
